a67b6e42a237157d367e2243a0f1e11bd54671f5
liyang <Administrator@AFOJE-805021532>
2020-01-02 16:39:39 +0800

    commit code

---
+++ go-rabbitmq-fanout/rabbitmq_client2/client.go
@@ -0,0 +1,44 @@
+package main
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://yuxxto56:123456@192.168.1.132:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	failOnError(err, "Failed to bind a queue")
+	msgs, err := ch.Consume(
+		"order_email", // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
+}
---
+++ go-rabbitmq-fanout/rabbitmq_server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+	"time"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://yuxxto56:123456@192.168.1.132:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+	//声明通道
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+	body := fmt.Sprintf("hello:%d",time.Now().Unix())
+
+	t := time.Now()
+	err = ch.Publish(
+	"log_exchange",   //exchange fanout
+	"",                    // routing key
+	false,            // mandatory
+	false,            // immediate
+	amqp.Publishing{
+		DeliveryMode: 2,
+		ContentType:  "text/plain",
+		Body:         []byte(body),
+	})
+	elapsed := time.Since(t)
+	failOnError(err, "Failed to publish a message")
+	log.Println("[x] Send Second:",elapsed)
+	log.Printf("[x] Sent %s", body)
+}
